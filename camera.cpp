84066f3fe17d5b525519a44429fcb140c866782e
Donald <shidefujiucimi@qq.com>
2025-02-24 17:37:51 +0800

    camera cpp

---
+++ camera.cpp
@@ -0,0 +1,130 @@
+#include <camera.h>
+using namespace std;
+
+Camera::Camera(glm::vec3 Positioninput) : Position(Positioninput) {
+    optical_center = Position + focal_length * Front;
+    view = glm::lookAt(Position, Position + Front, Up);
+    float half_height_y = focal_length * tan(glm::radians(fovy));
+    float half_width_x = half_height_y / resolutiony * resolutionx;
+    pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
+    canvas_width = 2 * half_width_x;
+    canvas_height = 2 * half_height_y;
+}
+Camera::Camera(glm::vec3 Positioninput, glm::vec3 Rotationinput) : Position(Positioninput), Rotation(Rotationinput) {
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::rotate(trans, glm::radians(Rotation.x), glm::vec3(1.0, 0.0, 0.0));
+    trans = glm::rotate(trans, glm::radians(Rotation.y), glm::vec3(0.0, 1.0, 0.0));
+    trans = glm::rotate(trans, glm::radians(Rotation.z), glm::vec3(0.0, 0.0, 1.0));
+
+    Front = trans * glm::vec4(Front, 1.0f);
+    Right = trans * glm::vec4(Right, 1.0f);
+    Up = trans * glm::vec4(Up, 1.0f);
+
+    optical_center = Position + focal_length * Front;
+    view = glm::lookAt(Position, Position + Front, Up);
+    float half_height_y = focal_length * tan(glm::radians(fovy));
+    float half_width_x = half_height_y / resolutiony * resolutionx;
+    pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
+    canvas_width = 2 * half_width_x;
+    canvas_height = 2 * half_height_y;
+}
+
+void Camera::SetPosition(glm::vec3 Positioninput) {
+    Position = Positioninput;
+    optical_center = Position + focal_length * Front;
+    pixel00_loc = optical_center + (canvas_height / 2) * Up - (canvas_width / 2) * Right;
+    view = glm::lookAt(Position, Position + Front, Up);
+
+}
+void Camera::SetRotation(glm::vec3 Rotationinput) {
+    Rotation = Rotationinput;
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::rotate(trans, glm::radians(Rotation.x), glm::vec3(1.0, 0.0, 0.0));
+    trans = glm::rotate(trans, glm::radians(Rotation.y), glm::vec3(0.0, 1.0, 0.0));
+    trans = glm::rotate(trans, glm::radians(Rotation.z), glm::vec3(0.0, 0.0, 1.0));
+
+    Front = trans * glm::vec4(Front, 1.0f);
+    Right = trans * glm::vec4(Right, 1.0f);
+    Up = trans * glm::vec4(Up, 1.0f);
+
+    optical_center = Position + focal_length * Front;
+    pixel00_loc = optical_center + (canvas_height / 2) * Up - (canvas_width / 2) * Right;
+    view = glm::lookAt(Position, Position + Front, Up);
+}
+void Camera::SetFocal(float focal) {
+    focal_length = focal;
+    optical_center = Position + focal_length * Front;
+    float half_height_y = focal_length / tan(glm::radians(fovy));
+    float half_width_x = half_height_y * resolutiony / resolutionx;
+    pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
+    canvas_width = 2 * half_width_x;
+    canvas_height = 2 * half_height_y;
+}
+void Camera::SetZoom(float fovyinput) {
+    fovy = fovyinput;
+    float half_height_y = focal_length / tan(glm::radians(fovy));
+    float half_width_x = half_height_y * resolutiony / resolutionx;
+    pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
+    canvas_width = 2 * half_width_x;
+    canvas_height = 2 * half_height_y;
+}
+void Camera::SetResolution(float x, float y) {
+    resolutionx = x;
+    resolutiony = y;
+    float half_height_y = focal_length / tan(glm::radians(fovy));
+    float half_width_x = half_height_y * resolutiony / resolutionx;
+    pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
+    canvas_width = 2 * half_width_x;
+    canvas_height = 2 * half_height_y;
+}
+void Camera::RenderToimage(hittable_list scene, const char* _Filename) {
+    ofstream outimage;
+    outimage.open(_Filename);
+    glm::vec3 color;//colorµÄ·¶Î§ÊÇ£¨0,1£©
+    glm::vec3 pixelposition = pixel00_loc;
+
+    outimage << "P3\n" << resolutionx << ' ' << resolutiony << "\n255\n";
+    for (int j = 0; j < resolutiony; j++) {
+        for (int i = 0; i < resolutionx; i++) {
+            glm::vec3 direction = pixelposition - Position;
+            color = Shade(Ray(Position, direction), 0.0, infinity, scene);
+            write_color(color, outimage);
+
+            pixelposition += (canvas_width / resolutionx) * Right;
+        }
+        pixelposition -= canvas_width * Right;
+        pixelposition -= (canvas_height / resolutiony) * Up;
+
+    }
+    outimage.close();
+    cout << "finish" << endl;
+}
+
+glm::vec3 Camera::calRight(glm::vec3 Front) {
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    return glm::normalize(glm::cross(up, Front));
+}
+void Camera::write_color(const glm::vec3& color, ofstream& outimage) {
+
+    int ir = int(256 * glm::clamp(double(color.r), 0.0, 1.0));
+    int ig = int(256 * glm::clamp(double(color.g), 0.0, 1.0));
+    int ib = int(256 * glm::clamp(double(color.b), 0.0, 1.0));
+
+    outimage << ir << ' ' << ig << ' ' << ib << '\n';
+}
+glm::vec3 Camera::Shade(const Ray& ray, double ray_tmin, double ray_tmax, const hittable_list& scene) {
+    glm::vec3 color;
+    Hit_record hit_record;
+    if (scene.hit(ray, ray_tmin, ray_tmax, hit_record)) {
+        color = glm::vec3(
+            (hit_record.normal.r + 1.0) * 0.5,
+            (hit_record.normal.g + 1.0) * 0.5,
+            (hit_record.normal.b + 1.0) * 0.5
+        );
+    }
+    else
+        color = glm::vec3(0.0, 0.0, 0.0);
+
+
+    return color;
+}
--- headfiles/camera.h
+++ headfiles/camera.h
@@ -6,6 +6,9 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <math.h>
+
+#include <hittable_list.h>
+#include <common_header.h>
 using namespace std;
 class Camera
 {
@@ -28,87 +31,20 @@ public:
     float canvas_width;
     float canvas_height;
     
-    Camera(glm::vec3 Positioninput) : Position(Positioninput){
-        optical_center = Position + focal_length * Front;
-        view = glm::lookAt(Position, Position + Front, Up);
-        float half_height_y = focal_length * tan(glm::radians(fovy));
-        float half_width_x = half_height_y  / resolutiony * resolutionx;
-        pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
-        canvas_width = 2 * half_width_x;
-        canvas_height = 2 * half_height_y;
-    }
-    Camera(glm::vec3 Positioninput,glm::vec3 Rotationinput): Position(Positioninput),Rotation(Rotationinput){
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::rotate(trans, glm::radians(Rotation.x), glm::vec3(1.0, 0.0, 0.0));
-        trans = glm::rotate(trans, glm::radians(Rotation.y), glm::vec3(0.0, 1.0, 0.0));
-        trans = glm::rotate(trans, glm::radians(Rotation.z), glm::vec3(0.0, 0.0, 1.0));
+    Camera(glm::vec3 Positioninput);
+    Camera(glm::vec3 Positioninput, glm::vec3 Rotationinput);
 
-        Front = trans * glm::vec4(Front,1.0f);
-        Right = trans * glm::vec4(Right, 1.0f);
-        Up = trans * glm::vec4(Up, 1.0f);
-
-        optical_center = Position + focal_length * Front;
-        view = glm::lookAt(Position, Position + Front, Up);
-        float half_height_y = focal_length * tan(glm::radians(fovy));
-        float half_width_x = half_height_y / resolutiony * resolutionx;
-        pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
-        canvas_width = 2 * half_width_x;
-        canvas_height = 2 * half_height_y;
-    }
-    void SetPosition(glm::vec3 Positioninput) {
-        Position = Positioninput;
-        optical_center = Position + focal_length * Front;
-        pixel00_loc = optical_center + (canvas_height / 2) * Up - (canvas_width / 2) * Right;
-        view = glm::lookAt(Position, Position + Front, Up);
-        
-    }
-    void SetRotation(glm::vec3 Rotationinput) {
-        Rotation = Rotationinput;
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::rotate(trans, glm::radians(Rotation.x), glm::vec3(1.0, 0.0, 0.0));
-        trans = glm::rotate(trans, glm::radians(Rotation.y), glm::vec3(0.0, 1.0, 0.0));
-        trans = glm::rotate(trans, glm::radians(Rotation.z), glm::vec3(0.0, 0.0, 1.0));
-
-        Front = trans * glm::vec4(Front, 1.0f);
-        Right = trans * glm::vec4(Right, 1.0f);
-        Up = trans * glm::vec4(Up, 1.0f);
-
-        optical_center = Position + focal_length * Front;
-        pixel00_loc = optical_center + (canvas_height / 2) * Up - (canvas_width / 2) * Right;
-        view = glm::lookAt(Position, Position + Front, Up);
-    }
-    void SetFocal(float focal) {
-        focal_length = focal;
-        optical_center = Position + focal_length * Front;
-        float half_height_y = focal_length / tan(glm::radians(fovy));
-        float half_width_x = half_height_y * resolutiony / resolutionx;
-        pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
-        canvas_width = 2 * half_width_x;
-        canvas_height = 2 * half_height_y;
-    }
-    void SetZoom(float fovyinput){
-        fovy = fovyinput;
-        float half_height_y = focal_length / tan(glm::radians(fovy));
-        float half_width_x = half_height_y * resolutiony / resolutionx;
-        pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
-        canvas_width = 2 * half_width_x;
-        canvas_height = 2 * half_height_y;
-    }
-    void SetResolution(float x, float y) {
-        resolutionx = x;
-        resolutiony = y;
-        float half_height_y = focal_length / tan(glm::radians(fovy));
-        float half_width_x = half_height_y * resolutiony / resolutionx;
-        pixel00_loc = optical_center + half_height_y * Up - half_width_x * Right;
-        canvas_width = 2 * half_width_x;
-        canvas_height = 2 * half_height_y;
-    }
+    void SetPosition(glm::vec3 Positioninput);
+    void SetRotation(glm::vec3 Rotationinput);
+    void SetFocal(float focal);
+    void SetZoom(float fovyinput);
+    void SetResolution(float x, float y);
+    void RenderToimage(hittable_list scene, const char* _Filename);
 
 private:
-    glm::vec3 calRight(glm::vec3 Front) {
-        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-        return glm::normalize(glm::cross(up, Front));
-    }
+    glm::vec3 calRight(glm::vec3 Front);
+    void write_color(const glm::vec3& color, ofstream& outimage);
+    glm::vec3 Shade(const Ray& ray, double ray_tmin, double ray_tmax, const hittable_list& scene);
 };
 
 #endif
