98d7e7f35c5113bd229ebd6fa9eeb09068b57a99
Donald <shidefujiucimi@qq.com>
2025-02-24 16:34:32 +0800

    random function

--- headfiles/common_header.h
+++ headfiles/common_header.h
@@ -2,6 +2,7 @@
 #define COMMON_HEADER_H
 
 #include <cmath>
+#include <random>
 #include <iostream>
 #include <fstream>
 #include <limits>
@@ -30,5 +31,15 @@ inline double degrees_to_radians(double degrees) {
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+inline double random_double(double min,double max) {
+    static std::uniform_real_distribution<double> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
 
 #endif
