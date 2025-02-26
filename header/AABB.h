b4b30cf34249325b3385e694b8ade2af8181d2a6
Donald <shidefujiucimi@qq.com>
2025-02-26 15:34:24 +0800

    aabb

--- header/AABB.h
+++ header/AABB.h
@@ -35,18 +35,17 @@ const interval interval::empty = interval(+infinity, -infinity);
 const interval interval::universe = interval(-infinity, +infinity);
 class aabb {
 public:
-    
     interval x, y, z;
 
     aabb(){} 
-    aabb((const interval& x, const interval& y, const interval& z): x(x), y(y), z(z) {}
-    aabb(const point3& a, const point3& b) {
+    aabb(const interval& x, const interval& y, const interval& z): x(x), y(y), z(z) {}
+    aabb(const glm::vec3& a, const glm::vec3& b) {
         // Treat the two points a and b as extrema for the bounding box, so we don't require a
         // particular minimum/maximum coordinate order.
 
-        x = (a[0] <= b[0]) ? interval(a[0], b[0]) : interval(b[0], a[0]);
-        y = (a[1] <= b[1]) ? interval(a[1], b[1]) : interval(b[1], a[1]);
-        z = (a[2] <= b[2]) ? interval(a[2], b[2]) : interval(b[2], a[2]);
+        x = (a.x <= b.x) ? interval(a.x, b.x) : interval(b.x, a.x);
+        y = (a.y <= b.y) ? interval(a.y, b.y) : interval(b.y, a.y);
+        z = (a.z <= b.z) ? interval(a.z, b.z) : interval(b.z, a.z);
     }
 
     const interval& axis_interval(int n) const {
@@ -54,17 +53,16 @@ public:
         if (n == 2) return z;
         return x;
     }
-
-    bool hit(const ray& r, interval ray_t) const {
-        const point3& ray_orig = r.origin();
-        const vec3& ray_dir = r.direction();
+    bool hit(const Ray& r, interval ray_t) const {
+        const glm::vec3& ray_orig = r.origin;
+        const glm::vec3& ray_dir = r.direction;
 
         for (int axis = 0; axis < 3; axis++) {
             const interval& ax = axis_interval(axis);
             const double adinv = 1.0 / ray_dir[axis];
 
-            auto t0 = (ax.min - ray_orig[axis]) * adinv;
-            auto t1 = (ax.max - ray_orig[axis]) * adinv;
+            double t0 = (ax.min - ray_orig[axis]) * adinv;
+            double t1 = (ax.max - ray_orig[axis]) * adinv;
 
             if (t0 < t1) {
                 if (t0 > ray_t.min) ray_t.min = t0;
